59bc6e3f6e6a2df3b3d9a8508e44354f8884fdf0
Kim KunHa <ayyjh321@gmail.com>
2024-03-10 21:45:28 +0900

    [level 3] Title: 산 모양 타일링, Time: 0.71 ms, Memory: 5.51 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.cpp
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.cpp
@@ -1,34 +1,29 @@
 #include <string>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
-vector<vector<int>> cache{100001, vector<int>{0,0}};
+int dp1[200000];
+int dp2[200000];
 
 int solution(int n, vector<int> tops) {
-    if(tops[0] > 0)
-    {
-        cache[0][0] = 3; cache[0][1] = 1;
-    }
-    else
-    {
-        cache[0][0] = 2; cache[0][1] = 1;
+    if (tops[0] == 0){
+        dp1[0] = 1;
+        dp2[0] = 2;
+    } else {
+        dp1[0] = 1;
+        dp2[0] = 3;
     }
 
-    for(int i = 1; i < n; i++)
-    {
-        if(tops[i] > 0)
-        {
-            cache[i][0] = (cache[i-1][0] * 3 + cache[i-1][1] * 2) % 10007;
-            cache[i][1] = (cache[i-1][0]  + cache[i-1][1]) % 10007;
-        }
-        else
-        {
-            cache[i][0] = (cache[i-1][0] * 2 + cache[i-1][1] ) % 10007;
-            cache[i][1] = (cache[i-1][0] + cache[i-1][1]) % 10007;
+    for (int i = 1; i < tops.size(); ++i){
+        if (tops[i] == 1){
+            dp1[i] = (dp1[i - 1] + dp2[i - 1]) % 10007;
+            dp2[i] = (dp1[i - 1] * 2 + dp2[i - 1] * 3) % 10007;
+        } else {
+            dp1[i] = (dp1[i - 1] + dp2[i - 1]) % 10007;
+            dp2[i] = (dp1[i - 1] + dp2[i - 1] * 2) % 10007;
         }
     }
 
-    return (cache[n-1][0] + cache[n-1][1])%10007;;
+    return (dp1[tops.size() - 1] + dp2[tops.size() - 1]) % 10007;
 }
