cb756d539abfb54ac7e46784fa48b6711d1392e3
Kim KunHa <ayyjh321@gmail.com>
2024-03-10 20:18:01 +0900

    [level 3] Title: 산 모양 타일링, Time: 0.51 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<vector<int>> cache{100001, vector<int>{0,0}};
+
+void dp(int n, vector<int> tops, int cur)
+{
+    
+}
+
+int solution(int n, vector<int> tops) {
+    if(tops[0] > 0)
+    {
+        cache[0][0] = 3; cache[0][1] = 1;
+    }
+    else
+    {
+        cache[0][0] = 2; cache[0][1] = 1;
+    }
+
+    for(int i = 1; i < n; i++)
+    {
+        if(tops[i] > 0)
+        {
+            cache[i][0] = (cache[i-1][0] * 3 + cache[i-1][1] * 2) % 10007;
+            cache[i][1] = (cache[i-1][0]  + cache[i-1][1]) % 10007;
+        }
+        else
+        {
+            cache[i][0] = (cache[i-1][0] * 2 + cache[i-1][1] ) % 10007;
+            cache[i][1] = (cache[i-1][0] + cache[i-1][1]) % 10007;
+        }
+    }
+
+    return (cache[n-1][0] + cache[n-1][1])%10007;;
+}
