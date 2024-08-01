f0f23d157b940dc763d6aed6a37a7574fcb88eca
Kim KunHa <ayyjh321@gmail.com>
2024-08-01 17:17:46 +0900

    [Gold V] Title: 용액, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int a[100'005];
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n;
+    for(int i=0; i<n; i++) cin>>a[i];
+    int ans=2*1e9+1;
+
+    int st=0;
+    int en=n-1;
+    int ans1, ans2;
+    while(st<en){
+        int cur=a[st]+a[en];
+        int num=abs(cur);
+        if(num<ans){
+            ans=num;
+            ans1=a[st];
+            ans2=a[en];
+        }
+        if(cur>0) en--;
+        else st++;
+    }
+    cout<<ans1<<' '<<ans2;
+}
