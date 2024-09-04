aaebb4d8ad89ccd77911585bab6a95b1f987f6dc
Kim KunHa <ayyjh321@gmail.com>
2024-09-04 11:32:41 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+    priority_queue<int> pq;
+    for(int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+        if(num == 0)
+        {
+            if(pq.empty())
+            {
+                cout << 0 << '\n';
+            }
+            else
+            {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+        }
+        else
+        {
+            pq.push(num);
+        }
+    }
+}
