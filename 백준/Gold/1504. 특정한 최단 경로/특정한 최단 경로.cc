13a957a6889a5f6bc4b3c31360f8eea18ba8d981
Kim KunHa <ayyjh321@gmail.com>
2024-10-11 11:18:57 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 44 ms, Memory: 5728 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
@@ -1,44 +1,59 @@
-#include<iostream>
-#include<algorithm>
+#include <bits/stdc++.h>
+#define cost first
+#define node second
+#define pii pair<int, int>
+
 using namespace std;
-#define MAX 801
 
-int grap[MAX][MAX];
-int N, E, a, b, c, num1, num2, INF = 987654321;
-bool answer = true;
+int n,e;
+vector<vector<pair<int, int>>> board(801); // node, cost
+int v1, v2;
 
-void floid() {
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
-			for (int a = 1; a <= N; a++) {
-				if (grap[j][a] > grap[j][i] + grap[i][a])
-					grap[j][a] = grap[j][i] + grap[i][a];
-			}
-		}
-	}
-
+int dijkstra(int root, int target)
+{
+    vector<int> dist(n+1, INT_MAX);
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    dist[root] = 0;
+    pq.push({0, root});
+    
+    while(!pq.empty())
+    {
+        auto cur = pq.top(); pq.pop();
+        
+        for(const auto& next : board[cur.node])
+        {
+            if(dist[next.node] > dist[cur.node] + next.cost)
+            {
+                dist[next.node] = dist[cur.node] + next.cost;
+                pq.push({dist[cur.node] + next.cost, next.node});
+            }
+        }
+    }
+    return dist[target];
 }
 
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-
-	cin >> N >> E;
-	for (int i = 0; i < E; i++) {
-		cin >> a >> b >> c;
-		grap[a][b] = c;
-		grap[b][a] = c;
-	}
-	cin >> num1 >> num2;
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
-			if (i == j) continue;
-			if (grap[i][j] == 0) grap[i][j] = 987654321;
-		}
-	}
-	floid();
-	if (grap[1][num1] == INF || grap[num1][num2] == INF || grap[num2][N] == INF) answer = false;
-	if(answer)
-		cout << min((grap[1][num1] + grap[num1][num2] + grap[num2][N]), (grap[1][num2] + grap[num2][num1] + grap[num1][N])) << "\n";
-	else cout << -1 << "\n";
+int main() 
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    cin >> n >> e;
+    for(int i = 0; i < e; i++)
+    {
+        int n1, n2, c;
+        cin >> n1 >> n2 >> c;
+        board[n1].push_back({c, n2});
+        board[n2].push_back({c, n1});
+    }
+    cin >> v1 >> v2;
+    int root2v1 = dijkstra(1, v1);
+    int v12v2 = dijkstra(v1, v2);
+    int v22n = dijkstra(v2, n);
+    if(root2v1 == INT_MAX || v12v2 == INT_MAX || v22n == INT_MAX)
+        cout << -1;
+    else
+        cout << min(root2v1 + v12v2 + v22n,
+            dijkstra(1,v2) + dijkstra(v2, v1) + dijkstra(v1, n));
+    return 0;
 }
