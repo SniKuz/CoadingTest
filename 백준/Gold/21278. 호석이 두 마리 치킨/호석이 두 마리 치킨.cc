7f4bc54a7a213be81e167be3b005660b21c19a45
Kim KunHa <ayyjh321@gmail.com>
2024-10-13 00:07:03 +0900

    [Gold V] Title: 호석이 두 마리 치킨, Time: 0 ms, Memory: 2020 KB, Score: 19 point -BaekjoonHub

--- 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.cc
+++ 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.cc
@@ -1,62 +1,48 @@
 #include <bits/stdc++.h>
+#define ll long long
 
 using namespace std;
-
+vector<vector<ll >> board(101, vector<ll >(101, INT_MAX));
 int n, m;
 
-vector<vector<int>> board(101, vector<int>(101, 2e8));
-int v1, v2, res = 2e8;
+ll res = INT_MAX;
+pair<int, int> node;
 
-int main()
+int main() 
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    
     cin >> n >> m;
-    for(int i = 1; i <= n; i++)
-        board[i][i] = 0;
     for(int i = 0; i < m; i++)
     {
         int a, b;
         cin >> a >> b;
-        board[a][b] = 1;
-        board[b][a] = 1;
+        board[a][b] = 2;
+        board[b][a] = 2;
     }
-
+    for(int i = 1; i <= n; i++)
+        board[i][i] = 0;
     for(int k = 1; k <= n; k++)
-    {
         for(int i = 1; i <= n; i++)
-        {
             for(int j = 1; j <= n; j++)
-            {
                 if(board[i][j] > board[i][k] + board[k][j])
-                {
                     board[i][j] = board[i][k] + board[k][j];
-                }
-            }
-        }
-    }
-
-    for(int i = n-1; i > 0; i--)
+                    
+    for(int i = n; i > 0; i--)
     {
-        for(int j = i+1; j > 0; j--)
+        for(int j = i-1; j > 0; j--)
         {
-            int ik = 0, jk = 0;
+            int cnt = 0;
             for(int k = n; k > 0; k--)
             {
-                if(board[i][k] < board[j][k])
-                    ik += board[i][k] * 2;
-                else
-                    jk += board[j][k] * 2;
+                cnt += min(board[i][k], board[j][k]);
             }
-            if(ik + jk <= res)
+            if(res >= cnt)
             {
-                res = ik + jk;
-                v1 = i;
-                v2 = j;
+                res = cnt;
+                node.first = j;
+                node.second = i;
             }
         }
     }
-    cout << v1 << ' ' << v2 << ' ' << res;
+    cout << node.first << ' ' << node.second << ' ' << res;
+    return 0;
 }
