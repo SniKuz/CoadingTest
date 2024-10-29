74e678500a73505ed06aecc8e0718e0cca5217c0
Kim KunHa <ayyjh321@gmail.com>
2024-10-29 11:39:38 +0900

    [Bronze III] Title: 현대모비스와 함께하는 부품 관리, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int t, n, a, b;
+
+
+int main() 
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> t;
+    for(int tc = 1; tc <= t; tc++)
+    {
+        cin >> n >> a >> b;
+        int sumU = 0, sumV = 0;
+        int u, v;
+        for(int i = 0; i < n; i++)
+        {
+            cin >> u >> v;
+        }
+        cout << "Material Management " << tc << '\n';
+        cout << "Classification ---- End!\n";
+    }
+    return 0;
+}
