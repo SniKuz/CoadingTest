c874b93bc4b222a403a744b910a934ba7be82a5a
Kim KunHa <ayyjh321@gmail.com>
2024-11-06 11:10:36 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() 
+{
+    double a, b;
+    cin >> a >> b;
+    cout << fixed;
+    cout.precision(12);
+    cout << a / b;
+    return 0;
+}
