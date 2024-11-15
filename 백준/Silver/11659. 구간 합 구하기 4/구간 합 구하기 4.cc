567aea24df5d58efc7a55981879c07f76040fe5a
Kim KunHa <ayyjh321@gmail.com>
2024-11-15 09:01:19 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 36 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+vector<int> v;
+vector<int> prefix;
+
+int main() 
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    cin >> n >> m;
+    v.resize(n);
+    prefix.resize(n+1);
+    for(int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+        prefix[i+1] = prefix[i] + v[i];
+    }
+    for(int i = 0; i < m; i++)
+    {
+        int l, r;
+        cin >> l >> r;
+        cout << prefix[r] - prefix[l-1] << '\n';
+    }
+    return 0;
+}
